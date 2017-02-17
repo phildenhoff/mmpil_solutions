3ada4c6dc169baf16e2f222c0f7d1c12bc061415
Phil Denhoff <phildenhoff@gmail.com>
2017-02-17 13:36:33 -0800

    Added decryption

--- programs/simple_ciphers.py
+++ programs/simple_ciphers.py
@@ -1,6 +1,8 @@
 """Implementation of Vignere and Caeser ciphers."""
 
 
+# STR_HOW_TO = ""
+
 def vignere(word):
     """Encrypt word with Vignere cihper."""
     print word
@@ -17,6 +19,8 @@ def caeser_letter(letter, difference):
     """Encrypt a letter using Caeser cipher."""
     if ord(letter)+difference > 122:
         return chr(ord(letter)+difference-26)
+    if ord(letter)+difference < 97:
+            return chr(ord(letter)+difference+26)
     else:
         return chr(ord(letter)+difference)
 
@@ -37,9 +41,15 @@ while inputOK != 'true':
         print words_ciphered
         break
     if userInput[0].lower() == "d":
-        print "    Sorry, decrypting is not yet available."
-        print "    However, feel free to continue encrypting"
-        # code would
-        # go here
+        print "    To decrypt using the Caeser Cipher, type a phrase and the\
+ offset you'd like to reverse on seperate lines."
+        userInput = raw_input().split()
+        userDiff = int(raw_input())
+        inputOK = 'true'
+        words_ciphered = ' '.join(list(map((lambda x: caeser(x, -userDiff)),
+                                           userInput)))
+        print "    Your decrypted phrase is"
+        print words_ciphered
+        break
     else:
         print "    You must input \"e\" or \"d\"."
