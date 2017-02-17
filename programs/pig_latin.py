0cd6b76b82ca1c8f133b111fb18a2160f635b4dd
Phil Denhoff <phildenhoff@gmail.com>
2017-02-16 17:17:30 -0800

    Fixed some whiny pep8 issues

--- programs/pig_latin.py
+++ programs/pig_latin.py
@@ -1,26 +1,28 @@
-"""Turn a given string into Pig Latin"""
+"""Turn a given string into Pig Latin."""
+
 
 def piggify(word):
-	# starts with vowek
-	if word[0].lower() in vowels:
-		return word+"way"
-	# starts with consonant cluster
-	if word[0:2].lower() in clusters:
-		return word[2:len(word)]+word[0:2]+"ay"
-	# starts with consonant
-	else:
-		return word[1:len(word)]+word[0]+"ay"
+    """Turn a given word into pig latin."""
+    # starts with vowel
+    if word[0].lower() in vowels:
+        return word+"way"
+    # starts with consonant cluster
+    if word[0:2].lower() in clusters:
+        return word[2:len(word)]+word[0:2]+"ay"
+    # starts with consonant
+    else:
+        return word[1:len(word)]+word[0]+"ay"
 
 
 print "Type a string to convert to pig latin"
 userInput = raw_input()
 wordList = userInput.split()
 vowels = ['a', 'e', 'i', 'o', 'u']
-clusters = ['ch', 'sh','sm']
+clusters = ['ch', 'sh', 'sm']
 piggedWordList = []
 
 for word in wordList:
-	piggedWordList.append(piggify(word))
+    piggedWordList.append(piggify(word))
 
 piggedSentence = " ".join(piggedWordList)
 
